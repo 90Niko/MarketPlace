ee4af96a7ddbdb61ed1fb5c991abc7fdf384755f
90Niko <110725784+90Niko@users.noreply.github.com>
2024-03-12 10:04:39 +0100

    Cleaning the IoC

--- MarketPlace/Program.cs
+++ MarketPlace/Program.cs
@@ -1,6 +1,4 @@
-using MarketPlace.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using MarketPlace.Extensions.DependencyInjection;
 
 namespace MarketPlace
 {
@@ -10,19 +8,16 @@ namespace MarketPlace
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddAplicationDbContext(builder.Configuration);
+            builder.Services.AddAplicationIdentity(builder.Configuration);
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddAplicationServices();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -42,9 +37,8 @@ namespace MarketPlace
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapDefaultControllerRoute();
+
             app.MapRazorPages();
 
             app.Run();
